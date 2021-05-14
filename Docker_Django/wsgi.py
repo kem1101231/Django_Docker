a20898f8b5eaf99a40e589850540d6d536e655c0
kris <kristopherc@codev.com>
2021-05-14 21:55:40 +0800

    Update mislabeled folders in django

--- Docker_Django/wsgi.py
+++ Docker_Django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Docker_Django_Files project.
+WSGI config for Docker_Django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Docker_Django_Files.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Docker_Django.settings")
 
 application = get_wsgi_application()
