5b3719e0dcf623f39ea44b612bcc44023e359d09
kris <kristopherc@codev.com>
2021-05-14 22:19:59 +0800

    Secret key Added

--- Docker_Django/settings.py
+++ Docker_Django/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '8#j#1js+gtf7l!^*@hd(gef5i9d=wl=jc2*z%%nozs)qdd#u0k'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
